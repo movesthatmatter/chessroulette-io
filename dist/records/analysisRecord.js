4ac6954abb96c14a47b449f71eb8f7d463df91e6
Gabriel C. Troia <catalin.troia@gmail.com>
2021-08-19 18:28:01 -0500

    Fix relative path

--- dist/records/analysisRecord.js
+++ dist/records/analysisRecord.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.analysisRecord = void 0;
 var io = require("io-ts");
 var io_ts_isodatetime_1 = require("io-ts-isodatetime");
-var chessGame_1 = require("src/chessGame");
+var chessGame_1 = require("../chessGame");
 exports.analysisRecord = io.type({
     id: io.string,
     createdAt: io_ts_isodatetime_1.isoDateTimeFromIsoString,
