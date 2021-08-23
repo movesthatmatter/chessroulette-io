ea9a086c60f2661a4ba8f6538f2590a5cdac1ca8
Gabriel C. Troia <catalin.troia@gmail.com>
2021-08-23 15:19:54 -0500

    Add drawable to AnalysisRecord

--- dist/records/analysisRecord.d.ts
+++ dist/records/analysisRecord.d.ts
@@ -1,9 +1,29 @@
 import * as io from 'io-ts';
-export declare const analysisRecord: io.TypeC<{
+export declare const chessBoardDrawShape: io.IntersectionC<[io.TypeC<{
+    orig: io.StringC;
+    visible: io.BooleanC;
+    defaultSnapToValidMove: io.BooleanC;
+    eraseOnClick: io.BooleanC;
+}>, io.PartialC<{
+    dest: io.StringC;
+    brush: io.StringC;
+}>]>;
+export declare type ChessBoardDrawShape = io.TypeOf<typeof chessBoardDrawShape>;
+export declare const analysisRecord: io.IntersectionC<[io.TypeC<{
     id: io.StringC;
     createdAt: io.Type<import("io-ts-isodatetime/dist/lib/ISODateTime").ISODateTimeBrand, string, unknown>;
     updatedAt: io.Type<import("io-ts-isodatetime/dist/lib/ISODateTime").ISODateTimeBrand, string, unknown>;
     history: io.Type<import("../chessGame").ChessRecursiveHistory, import("../chessGame").ChessRecursiveHistory, unknown>;
     focusIndex: io.Type<import("../chessGame").ChessHistoryIndex, import("../chessGame").ChessHistoryIndex, unknown>;
-}>;
+}>, io.TypeC<{
+    drawable: io.ArrayC<io.IntersectionC<[io.TypeC<{
+        orig: io.StringC;
+        visible: io.BooleanC;
+        defaultSnapToValidMove: io.BooleanC;
+        eraseOnClick: io.BooleanC;
+    }>, io.PartialC<{
+        dest: io.StringC;
+        brush: io.StringC;
+    }>]>>;
+}>]>;
 export declare type AnalysisRecord = io.TypeOf<typeof analysisRecord>;
