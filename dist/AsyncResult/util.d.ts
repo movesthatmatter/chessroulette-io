a044be7916e21c2e7ca97e92ab23f47fc2f05335
Gabriel C. Troia <catalin.troia@gmail.com>
2021-09-23 20:49:59 -0500

    Reset the dist folder

--- dist/AsyncResult/util.d.ts
+++
@@ -1,4 +0,0 @@
-import { IsExact } from 'conditional-type-checks';
-export declare function isExactType<A, B>(x: IsExact<A, B>): void;
-export declare const delay: (ms?: number) => Promise<unknown>;
-export declare const traceAsyncErrors: <T>(fn: () => Promise<T>) => Promise<T>;
